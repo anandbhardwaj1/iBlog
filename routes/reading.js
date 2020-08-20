aa3937fa08ca1f88c48ab72b1dfe40026a61ed34
Anand Raj <68288054+anandbhardwaj1@users.noreply.github.com>
2020-08-20 11:09:04 +0530

    Update reading.js

--- routes/reading.js
+++ routes/reading.js
@@ -4,12 +4,16 @@ const bodyParser=require("body-parser");
 var Blog = require("../models/blog.js");
 var Node = require("../models/node.js");
 
+
+const { body, validationResult } = require('express-validator');
 router.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
 
+
+Blog.find().sort({"rating" : -1});
 
 
 
@@ -82,8 +86,23 @@ router.get("/load/:id", function (req, res) {
   });
 });
 
-  router.post("/new/:id",function(req,res)
-  { var num1=Number(req.body.rating);
+  router.post("/new/:id",  [
+
+  body('rating').isNumeric({ min: 0,max :5})
+], function(req,res)
+  {
+    const errors=validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+
+        res.send("Invalid Rating");
+      console.log(errors);
+      //  console.log(req.body.nodes[0].content);
+
+    }
+else
+{
+     var num1=Number(req.body.rating);
 
 let id=req.param.id;
 
@@ -94,14 +113,17 @@ Blog.findByIdAndUpdate(
    {upsert:false},
 
    function(err, document){
-   
+
+
 });
-   res.redirect("/");
 
 
+       res.redirect("/");
+
+}
   });
 
-  
+
 
 
 
